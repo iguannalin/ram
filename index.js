af65cfe92bd24961002a01be6aac3c3b1b98ee9e
seasons986 <annalin12345@gmail.com>
2024-01-10 04:20:46 +0900

    cleanup

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ window.addEventListener("load", () => {
   let os = 300;
   let time = 500;
   
-  function createWindow(mX, mY) {
+  function createWindow() {
     const text = `<!DOCTYPE html><html> <head> <title>keep</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://iguannalin.github.io/keep/index.css"/></head> <body><div id="container"><div id="cursor"></div></div></body><script>let c=document.getElementById("cursor");cTop=-75;setInterval(() => {c.style.top=cTop+'px';cTop=cTop<100?cTop+1:-75;},50);setInterval(() => window.moveTo(Math.random()*1000,Math.random()*1000), ${time});</script></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
@@ -17,8 +17,6 @@ window.addEventListener("load", () => {
     window.URL.revokeObjectURL(blobUrl);
   }
   
-  // setInterval(() => {
-    createWindow(window.screenLeft, window.screenTop)//, getRandomInt(0,1000));
-    setInterval(() => {if (Math.random() > 0.8) location.reload()}, getRandomInt(100,1000));
-  // }, time);
+  createWindow();
+  setInterval(() => {if (Math.random() > 0.8) location.reload()}, getRandomInt(100,1000));
 });
